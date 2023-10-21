b2f9e1177c1ca7efa993c96231a5a513f3153167
Trần Thái Hướng <tranthaihuong8@gmail.com>
2023-10-21 13:50:10 +0700

    Initial commit

---
+++ Controllers/DetailProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanQuanAo.Controllers
+{
+    public class DetailProductController : Controller
+    {
+        // GET: DetailProduct
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
